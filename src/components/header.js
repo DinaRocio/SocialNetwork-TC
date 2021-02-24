26b487c4b30396412fe55a6e4e404f9887fa5580
Dina Rocio <dinarociovq15@hotmail.com>
2021-02-23 19:09:50 -0500

    Header component and styles is done

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../stylesheets/Header.css"
+
+function Header(){
+  return (
+    <header className="header">
+    <div className="left-elements">
+      <a href="#home" className="js-to-home-link">
+        <i className="fas fa-home"></i>
+      </a>
+      <div className="bars">
+        <span className="long_bar"></span>
+        <span className="small_bar"></span>
+      </div>
+    </div>
+    <div className="profile_icon">YL</div>
+  </header>
+  )
+}
+export default Header
